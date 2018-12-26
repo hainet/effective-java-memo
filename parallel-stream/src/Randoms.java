50c5ce9f432523bd731b6d69525fa6250444d1c6
HaineTakano <haine.takano@g.softbank.co.jp>
2018-12-26 18:51:46 +0900

    Polish

--- parallel-stream/src/Randoms.java
+++ parallel-stream/src/Randoms.java
@@ -1,12 +1,14 @@
 import java.math.BigInteger;
 import java.util.Random;
 import java.util.SplittableRandom;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Randoms {
 
     public static void main(final String[] args) {
-        splittableRandom();
         random();
+        splittableRandom();
+        threadLocalRandom();
     }
 
     private static void random() {
@@ -25,6 +27,18 @@ public class Randoms {
         final long start = System.currentTimeMillis();
 
         System.out.println(new SplittableRandom().ints(10_000_000, 0, 10_000_000)
+                .parallel()
+                .mapToObj(BigInteger::valueOf)
+                .filter(i -> i.isProbablePrime(50))
+                .count());
+
+        System.out.println("Processed in " + (System.currentTimeMillis() - start) + "ms");
+    }
+
+    private static void threadLocalRandom() {
+        final long start = System.currentTimeMillis();
+
+        System.out.println(ThreadLocalRandom.current().ints(10_000_000, 0, 10_000_000)
                 .parallel()
                 .mapToObj(BigInteger::valueOf)
                 .filter(i -> i.isProbablePrime(50))
